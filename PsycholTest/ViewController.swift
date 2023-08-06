48a59a27c1d139f169019e76dfdc3222acf202d7
Jeanine Chuang <amazingwiplala@gmail.com>
2023-08-06 20:36:21 +0800

    Initial Commit

---
+++ PsycholTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PsycholTest
+//
+//  Created by Jeanine Chuang on 2023/8/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
